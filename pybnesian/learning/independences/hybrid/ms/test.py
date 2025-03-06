numerical fixes & hyperparams

pybnesian/learning/independences/hybrid/ms/test.py
@@ -11,8 +11,8 @@ import numpy as np
 # In[2]:
 
 
-df = pd.read_csv("abalone.data", sep=None, engine='python', na_values='?')
 df = pd.read_csv("asia10K.csv", sep=None, engine='python', na_values='?')
+df = pd.read_csv("abalone.data", sep=None, engine='python', na_values='?')
 index_constant = np.where(df.nunique() == 1)[0]
 constant_columns = [df.columns[i] for i in index_constant]
 df = df.drop(columns=constant_columns)
@@ -34,7 +34,7 @@ import pybnesian as pbn
 # In[4]:
 
 
-mskcmi = pbn.MSKMutualInformation(df=df, k=5, samples=20, min_max_scale=True)
+mskcmi = pbn.MSKMutualInformation(df=df, k=100, seed=42, samples=10, scaling="normalized_rank",gamma_approx=True, adaptive_k=True)
 
 
 # In[ ]: